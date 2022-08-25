afcd5294acec94683bf303acf0c4229675681478
abhijitgawai <abhijit.gawai18@vit.edu>
2022-08-25 18:34:45 +0530

    init

---
+++ contracts/core/NewContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "../../interfaces/INewContract.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract NewContract{
+
+}
---
+++ tests/staking_interface/test_staking_interface.py
@@ -0,0 +1,4 @@
+from brownie import Wei, reverts, accounts, chain
+
+def test_name(isolation, owner, alice):
+    pass
